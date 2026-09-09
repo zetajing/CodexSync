7352800e2b0e922b0b6028fbd32a8e93f27ca089
zetajing <73131130+zetajing@users.noreply.github.com>
2026-09-09 13:24:01 +0800

    fix: add common global usings

---
+++ GlobalUsings.cs
@@ -0,0 +1,13 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Diagnostics;
+global using System.IO;
+global using System.IO.Compression;
+global using System.Linq;
+global using System.Net;
+global using System.Net.Http;
+global using System.Net.Http.Headers;
+global using System.Text;
+global using System.Text.Json;
+global using System.Threading;
+global using System.Threading.Tasks;
